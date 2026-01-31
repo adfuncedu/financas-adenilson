63c2e3ad8eec6671c917eea0d55d1b7e47b40536
adfuncedu <ad.func.edu@gmail.com>
2026-01-31 13:12:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -296,7 +296,7 @@ st.markdown("---")
 
 # 4. GRÁFICOS INTELIGENTES
 # ------------------------------------------------------------------------------
-tab1, tab2, tab3 = st.tabs(["📈 Evolução do Saldo", "📊 Fluxo Diário", "📂 Detalhes"])
+tab1, tab2, tab3 = st.tabs(["📈 Evolução do Saldo", "📊 Fluxo Diário", "📂 Extrato"])
 
 with tab1:
     st.subheader(f"Evolução: {texto_saldo}")
